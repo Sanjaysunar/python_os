00539a0a0875e19e70c0dc9943ea285bf1d663c7
Sanjay Sunar <sansunar11@gmail.com>
2022-08-29 21:26:54 +0530

    Update simp_match1.py

--- bs_reg_exp/simp_match1.py
+++ bs_reg_exp/simp_match1.py
@@ -4,5 +4,7 @@ print(re.search(r"^x", "xenon"))
 
 print(re.search(r"p.ng", "penguin"))
 
-print(re.search(r"p.ng".,"clapping"))
+print(re.search(r"p.ng","clapping"))
+
+print(re.search(r"p.ng","Pangaea", re.IGNORECASE))
 
